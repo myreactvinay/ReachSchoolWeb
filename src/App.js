c8ffaa150b20fd1911cd4a579711f3905591ecd7
Vinay Kumar Tanakala <v-vintan@microsoft.com>
2020-04-20 21:49:03 +0530

    ReactSchool

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Hero from "./Components/Hero.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hero />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero.js
@@ -0,0 +1,63 @@
+import React from "react";
+import data from "../data.json";
+
+const socialmedia = data.SocialMedias;
+class Hero extends React.Component {
+  render() {
+    return (
+      <div>
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <ol className="carousel-indicators">
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="0"
+              class="active"
+            ></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            {socialmedia.map((imgsrc, index) => (
+              <div
+                className={
+                  index === 0 ? "carousel-item active" : "carousel-item"
+                }
+              >
+                <img className="d-block w-100" src={imgsrc} alt="First slide" />
+              </div>
+            ))}
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+export default Hero;
