7eedfc4490f3b4c3ca77f4a62baf6110df8a3c28
Vinay Kumar Tanakala <v-vintan@microsoft.com>
2020-04-23 20:45:41 +0530

    Updates

---
+++ precache-manifest.8981adcf366c017f29700cc011115a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ce44cf53dbd8f2f3b33b2c7b45f7bf",
+    "url": "/ReachSchoolWeb/index.html"
+  },
+  {
+    "revision": "3ce5d7e447d910ab0997",
+    "url": "/ReachSchoolWeb/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "36e5ef52f3b3f6a2d6c4",
+    "url": "/ReachSchoolWeb/static/js/2.688b6198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReachSchoolWeb/static/js/2.688b6198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce5d7e447d910ab0997",
+    "url": "/ReachSchoolWeb/static/js/main.30de9720.chunk.js"
+  },
+  {
+    "revision": "d0d9bdc4a89681da0cec",
+    "url": "/ReachSchoolWeb/static/js/runtime-main.4763476a.js"
+  }
+]);
